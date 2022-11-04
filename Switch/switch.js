553e4041b48c7e54fd9e5884de4d023bae2bb9f6
Thamasha Amarasekara <thamashaamarasekara@Thamashas-MacBook-Pro.local>
2022-11-04 15:25:41 -0700

    Week9Upload

---
+++ Switch/switch.js
@@ -0,0 +1,15 @@
+var car = 0; 
+var tetx = ""; 
+
+switch(car) {
+    case 0: 
+        text = "off"; 
+        break; 
+    case 1: 
+        text = "0"; 
+        break; 
+    default:
+        text = "No value found"; 
+}
+
+console.log("The care is " + text); 
